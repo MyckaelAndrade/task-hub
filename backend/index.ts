bdc2e1aa906a09961ae002825cfcf4268d77eb97
MyckaelAndrade <myckael.and@gmail.com>
2025-01-18 19:58:08 -0300

    create project structure

---
+++ backend/index.ts
@@ -0,0 +1,48 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import routes from './src/routes/index';
+import './src/database/connection.ts';
+import bodyParser from 'body-parser';
+import AppError from './src/api/middlewares/AppError';
+import cors from 'cors';
+
+const app = express();
+
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000/', 
+  }),
+);
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(routes);
+
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.code).json({
+        code: error.code,
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(Server is running on http://localhost:${PORT});
+});
