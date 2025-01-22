f8643e6ad1ff5ba3b26acd191f17d1d2c950b021
MyckaelAndrade <myckael.and@gmail.com>
2025-01-22 15:33:17 -0300

    feat(frontend): connect frontend to backend API

--- frontend/task-hub/src/index.js
+++ frontend/task-hub/src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<<<<<<< Updated upstream
+=======
+import { BrowserRouter } from 'react-router-dom';
+>>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
+<<<<<<< Updated upstream
 );
+=======
+);
+>>>>>>> Stashed changes
